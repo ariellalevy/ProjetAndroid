e384ab4cab822b962dac5840d167064607ec22cd
Ariella LEVY <ariellalevy78@gmail.com>
2019-05-26 21:55:41 +0200

    ajout stockage

--- app/src/main/java/com/example/ariellalevy/projetandroid_levy_ariella_grp31/controler/Controller.java
+++ app/src/main/java/com/example/ariellalevy/projetandroid_levy_ariella_grp31/controler/Controller.java
@@ -1,11 +1,17 @@
 package com.example.ariellalevy.projetandroid_levy_ariella_grp31.controler;
 
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+
 import com.example.ariellalevy.projetandroid_levy_ariella_grp31.HarryPotterRestAPI;
 import com.example.ariellalevy.projetandroid_levy_ariella_grp31.model.HarryPotterCharacters;
 import com.example.ariellalevy.projetandroid_levy_ariella_grp31.vue.MainActivity;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -18,9 +24,11 @@ public class Controller implements Callback<List<HarryPotterCharacters>> {
 
     static final String BASE_URL = "https://raw.githubusercontent.com/ariellalevy/ariellalevy.github.io/master/";
     private MainActivity view;
+    private SharedPreferences sharedPreferences;
 
-    public Controller(MainActivity view) {
+    public Controller(MainActivity view, SharedPreferences sharedPreferences) {
         this.view = view;
+        this.sharedPreferences = sharedPreferences;
     }
 
     public void start() {
@@ -44,6 +52,7 @@ public class Controller implements Callback<List<HarryPotterCharacters>> {
     public void onResponse(Call<List<HarryPotterCharacters>> call, Response<List<HarryPotterCharacters>> response) {
         if(response.isSuccessful()) {
             List<HarryPotterCharacters> HarryPotterList = response.body();
+            storeData(HarryPotterList);
             view.showList(HarryPotterList);
         } else {
             System.out.println(response.errorBody());
@@ -52,6 +61,27 @@ public class Controller implements Callback<List<HarryPotterCharacters>> {
 
     @Override
     public void onFailure(Call<List<HarryPotterCharacters>> call, Throwable t) {
+        List<HarryPotterCharacters> HarryPotterList = getDataFromCache();
+        view.showList(HarryPotterList);
         t.printStackTrace();
+    }
+
+    private void storeData(List<HarryPotterCharacters> HarryPotterList) {
+        Gson gson = new Gson();
+        String HarryPotterListString = gson.toJson(HarryPotterList);
+        sharedPreferences
+                .edit()
+                .putString("cle_string", HarryPotterListString)
+                .apply();
+    }
+
+    private List<HarryPotterCharacters> getDataFromCache() {
+        String HarryPotterListString = sharedPreferences.getString("cle_string", "");
+        if(HarryPotterListString != null && !TextUtils.isEmpty(HarryPotterListString)){
+            Type listType = new TypeToken<List<HarryPotterCharacters>>(){}.getType();
+            List<HarryPotterCharacters> HarryPotterList = new Gson().fromJson(HarryPotterListString, listType);
+            return HarryPotterList;
+        }
+        return new ArrayList<>();
     }
 }
