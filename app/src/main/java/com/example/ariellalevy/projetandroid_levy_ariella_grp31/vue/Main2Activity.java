64bc749d3141b53529aff36a23ebdccea36002f6
Ariella LEVY <ariellalevy78@gmail.com>
2019-05-26 22:30:39 +0200

    test

---
+++ app/src/main/java/com/example/ariellalevy/projetandroid_levy_ariella_grp31/vue/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.example.ariellalevy.projetandroid_levy_ariella_grp31.vue;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.ariellalevy.projetandroid_levy_ariella_grp31.R;
+import com.example.ariellalevy.projetandroid_levy_ariella_grp31.controler.Controller;
+
+public class Main2Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
