da0513fd18604b384143cad22eb74f0ea816301a
Ariella LEVY <ariellalevy78@gmail.com>
2019-05-26 16:11:40 +0200

    add rest API

---
+++ app/src/main/java/com/example/ariellalevy/projetandroid_levy_ariella_grp31/model/HarryPotterCharacters.java
@@ -0,0 +1,148 @@
+package com.example.ariellalevy.projetandroid_levy_ariella_grp31.model;
+
+public class HarryPotterCharacters {
+    String name;
+    String species;
+    String gender;
+    String house;
+    String dateOfBirth;
+    //int yearOfBirth;
+    String ancestry;
+    String eyeColour;
+    String hairColour;
+    //Wand wand;
+    String patronus;
+    Boolean hogwartsStudent;
+    Boolean hogwartsStaff;
+    String actor;
+    Boolean alive;
+    String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /*public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }*/
+
+    public String getAncestry() {
+        return ancestry;
+    }
+
+    public void setAncestry(String ancestry) {
+        this.ancestry = ancestry;
+    }
+
+    public String getEyeColour() {
+        return eyeColour;
+    }
+
+    public void setEyeColour(String eyeColour) {
+        this.eyeColour = eyeColour;
+    }
+
+    public String getHairColour() {
+        return hairColour;
+    }
+
+    public void setHairColour(String hairColour) {
+        this.hairColour = hairColour;
+    }
+
+    /*public Wand getWand() {
+        return wand;
+    }
+
+    public void setWand(Wand wand) {
+        this.wand = wand;
+    }*/
+
+    public String getPatronus() {
+        return patronus;
+    }
+
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+
+    public Boolean getHogwartsStudent() {
+        return hogwartsStudent;
+    }
+
+    public void setHogwartsStudent(Boolean hogwartsStudent) {
+        this.hogwartsStudent = hogwartsStudent;
+    }
+
+    public Boolean getHogwartsStaff() {
+        return hogwartsStaff;
+    }
+
+    public void setHogwartsStaff(Boolean hogwartsStaff) {
+        this.hogwartsStaff = hogwartsStaff;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public Boolean getAlive() {
+        return alive;
+    }
+
+    public void setAlive(Boolean alive) {
+        this.alive = alive;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
